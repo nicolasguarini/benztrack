1613b93b391f53bf0caba01c1c27cbd9f74fd59d
Nicolas Guarini <nikiguarincia@gmail.com>
2022-04-22 13:28:42 +0200

    Create app's splash screen

--- app/src/main/java/it/pdm/benztrack/MainActivity.kt
+++ app/src/main/java/it/pdm/benztrack/MainActivity.kt
@@ -1,5 +1,6 @@
 package it.pdm.benztrack
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val sharedPref = getSharedPreferences("it.pdm.benztrack", Context.MODE_PRIVATE)
+
+        if(sharedPref.getBoolean("firstStart", true)){
+            println("First Start")
+            //TODO: Lanciare l'activity di benvenuto
+        }else{
+            println("Normal Launch")
+            //TODO: Lanciare l'activity della dashboard
+        }
     }
 }
