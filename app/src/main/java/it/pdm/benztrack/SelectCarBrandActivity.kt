c5f5a5003857ee7e2d1784411f6ca7abdad776c6
Nicolas Guarini <nikiguarincia@gmail.com>
2022-04-23 22:24:07 +0200

    Create SelectCarBrandActivity

--- app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
@@ -2,10 +2,39 @@ package it.pdm.benztrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 
 class SelectCarBrandActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_car_brand)
+
+        val users = arrayOf(
+            "BMW",
+            "Audi",
+            "Toyota",
+            "Honda",
+            "Hyundai",
+            "Mazda",
+            "Maserati",
+            "FIAT",
+            "Opel",
+            "Renault",
+            "Ferrari",
+            "Ford",
+            "Skoda",
+            "Bentley",
+            "Jaguar",
+            "Mercedes",
+            "McLaren",
+            "Subaru",
+            "Nissan",
+            "Dodge"
+        )
+
+        val listView = findViewById<ListView>(R.id.listViewCarBrands)
+        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, users)
+        listView.choiceMode = ListView.CHOICE_MODE_SINGLE
     }
 }
