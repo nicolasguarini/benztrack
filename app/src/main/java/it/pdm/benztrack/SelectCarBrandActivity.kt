3e33ecaf436c9a4baf67b7aa2b4113cb2fe66e05
Redon Kokaj <rkokaj@studenti.uninsubria.it>
2022-04-24 14:48:40 +0200

    Added car producer filter

--- app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
@@ -1,9 +1,12 @@
 package it.pdm.benztrack
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
 
 class SelectCarBrandActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +37,27 @@ class SelectCarBrandActivity : AppCompatActivity() {
         )
 
         val listView = findViewById<ListView>(R.id.listViewCarBrands)
-        listView.adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, users)
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, users)
+        listView.adapter = adapter
         listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+        val textFilter = findViewById<EditText>(R.id.producerFilter)
+        textFilter.addTextChangedListener(object:TextWatcher{
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+            }
+
+            override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
+                adapter.filter.filter(charSequence.toString())
+            }
+
+            override fun afterTextChanged(editable: Editable) {
+            }
+        })
+
+        findViewById<Button>(R.id.btnProducerNext).setOnClickListener {
+            val intent = Intent(this, SelectCarModelActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
+
---
+++ app/src/main/java/it/pdm/benztrack/SelectCarModelActivity.kt
@@ -0,0 +1,11 @@
+package it.pdm.benztrack
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SelectCarModelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_select_car_model)
+    }
+}
