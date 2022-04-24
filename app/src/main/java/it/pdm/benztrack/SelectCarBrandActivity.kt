e09d5609271f08c3306e4c75667fec99af6a6577
Nicolas Guarini <nikiguarincia@gmail.com>
2022-04-24 21:51:48 +0200

    Selected brand is fetched and passed to SelectCarModelActivity

--- app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
@@ -1,6 +1,5 @@
 package it.pdm.benztrack
 
-import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
@@ -9,11 +8,13 @@ import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class SelectCarBrandActivity : AppCompatActivity() {
+    private var selectedBrand = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_car_brand)
 
-        val users = arrayOf(
+        val users = arrayOf( //TODO: Get data from API
             "BMW",
             "Audi",
             "Toyota",
@@ -43,20 +44,27 @@ class SelectCarBrandActivity : AppCompatActivity() {
 
         val textFilter = findViewById<EditText>(R.id.producerFilter)
         textFilter.addTextChangedListener(object:TextWatcher{
-            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
-            }
+            override fun beforeTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {}
 
             override fun onTextChanged(charSequence: CharSequence, i: Int, i1: Int, i2: Int) {
                 adapter.filter.filter(charSequence.toString())
             }
 
-            override fun afterTextChanged(editable: Editable) {
-            }
+            override fun afterTextChanged(editable: Editable) {}
         })
 
+        listView.setOnItemClickListener { adapterView, _, position, _ ->
+            selectedBrand = adapterView.getItemAtPosition(position).toString()
+        }
+
         findViewById<Button>(R.id.btnProducerNext).setOnClickListener {
-            val intent = Intent(this, SelectCarModelActivity::class.java)
-            startActivity(intent)
+            if(selectedBrand != ""){
+                val intent = Intent(this, SelectCarModelActivity::class.java)
+                intent.putExtra("selectedBrand", selectedBrand)
+                startActivity(intent)
+            }else{
+                Toast.makeText(this, "No item selected!", Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
--- app/src/main/java/it/pdm/benztrack/SelectCarModelActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarModelActivity.kt
@@ -2,10 +2,15 @@ package it.pdm.benztrack
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class SelectCarModelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_car_model)
+
+        val selectedBrand = intent.getStringExtra("selectedBrand")
+
+        //TODO: Implement SelectCarModelActivity logic
     }
 }
