9479f0d2dcef44229c26a3fa92bb5e0288bee138
Redon Kokaj <rkokaj@studenti.uninsubria.it>
2022-04-26 17:40:25 +0200

    Added fuel type selection logic

--- app/src/main/java/it/pdm/benztrack/SelectCarFuelTypeActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarFuelTypeActivity.kt
@@ -1,7 +1,15 @@
 package it.pdm.benztrack
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import android.widget.ToggleButton
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.button.MaterialButtonToggleGroup
+import org.w3c.dom.Text
 
 class SelectCarFuelTypeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -10,7 +18,28 @@ class SelectCarFuelTypeActivity : AppCompatActivity() {
 
         val selectedBrand = intent.getStringExtra("selectedBrand").toString()
         val selectedModel = intent.getStringExtra("selectedModel").toString()
+        val toggleGroup = findViewById<MaterialButtonToggleGroup>(R.id.toggleButtonGroup)
+        var selectedFuel = ""
 
-        //TODO: Implement logic
+        toggleGroup.addOnButtonCheckedListener{toggleGroup, checkedId, isChecked ->
+            if(isChecked) {
+                when(checkedId) {
+                    R.id.diesel -> selectedFuel = findViewById<MaterialButton>(R.id.diesel).text.toString()
+                    R.id.gasoil -> selectedFuel = findViewById<MaterialButton>(R.id.gasoil).text.toString()
+                    R.id.hybrid -> selectedFuel = findViewById<MaterialButton>(R.id.hybrid).text.toString()
+                }
+            }
+            else {
+                selectedFuel = ""
+            }
+        }
+
+        findViewById<Button>(R.id.btnFuelNext).setOnClickListener {
+            if(selectedFuel != ""){
+
+            }else{
+                Toast.makeText(this, "No item selected!", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 }
