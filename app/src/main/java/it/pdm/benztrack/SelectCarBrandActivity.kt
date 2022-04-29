df4950604b4c9f92f5c6c274a740e9fc6d5d0fa5
Nicolas Guarini <nikiguarincia@gmail.com>
2022-04-29 21:48:36 +0200

    Progress bar implemented

--- app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
+++ app/src/main/java/it/pdm/benztrack/SelectCarBrandActivity.kt
@@ -1,10 +1,14 @@
 package it.pdm.benztrack
 
 import android.content.Intent
+import android.opengl.Visibility
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.text.Editable
 import android.text.TextWatcher
 import android.util.Log
+import android.view.View
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import com.google.gson.Gson
@@ -14,25 +18,61 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 import org.json.JSONObject
 import java.net.URL
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class SelectCarBrandActivity : AppCompatActivity() {
     private var selectedBrand = ""
     val url = URL("https://car-data.p.rapidapi.com/cars/makes")
     private var brands : Array<String> = emptyArray()
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_select_car_brand)
 
-        //TODO: show loader until getBrands() terminates
+        val service = Executors.newSingleThreadExecutor()
+        val handler = Handler(Looper.getMainLooper())
+        service.execute(Runnable {
+            getBrands()
+            handler.post(Runnable { showListView() })
+        })
 
-        getBrands()
+        findViewById<Button>(R.id.btnProducerNext).setOnClickListener {
+            if (selectedBrand != "") {
+                val intent = Intent(this, SelectCarModelActivity::class.java)
+                intent.putExtra("selectedBrand", selectedBrand)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "No item selected!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun getBrands(){
+        try {
+            val client = OkHttpClient()
+            val request = Request.Builder()
+                .url("https://car-data.p.rapidapi.com/cars/makes")
+                .get()
+                .addHeader("X-RapidAPI-Host", "car-data.p.rapidapi.com")
+                .addHeader("X-RapidAPI-Key", "09b1cc4c05msh7ee26c674f810e5p1df3e2jsn2e0a7d20d96b")
+                .build()
+
+            val body = client.newCall(request).execute().body?.string()
+            this.brands = GsonBuilder().create().fromJson(body, Array<String>::class.java)
+        } catch (e: java.lang.Exception) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun showListView(){
+        findViewById<ProgressBar>(R.id.progressBarCarBrands).visibility = View.GONE
 
         val listView = findViewById<ListView>(R.id.listViewCarBrands)
         val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, brands)
         listView.adapter = adapter
         listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+        listView.visibility = View.VISIBLE
 
         val textFilter = findViewById<EditText>(R.id.producerFilter)
         textFilter.addTextChangedListener(object : TextWatcher {
@@ -48,40 +88,6 @@ class SelectCarBrandActivity : AppCompatActivity() {
         listView.setOnItemClickListener { adapterView, _, position, _ ->
             selectedBrand = adapterView.getItemAtPosition(position).toString()
         }
-
-        findViewById<Button>(R.id.btnProducerNext).setOnClickListener {
-            if (selectedBrand != "") {
-                val intent = Intent(this, SelectCarModelActivity::class.java)
-                intent.putExtra("selectedBrand", selectedBrand)
-                startActivity(intent)
-            } else {
-                Toast.makeText(this, "No item selected!", Toast.LENGTH_SHORT).show()
-            }
-        }
-    }
-
-    private fun getBrands(){
-        val thread = Thread {
-            try {
-                val client = OkHttpClient()
-                val request = Request.Builder()
-                    .url("https://car-data.p.rapidapi.com/cars/makes")
-                    .get()
-                    .addHeader("X-RapidAPI-Host", "car-data.p.rapidapi.com")
-                    .addHeader("X-RapidAPI-Key", "09b1cc4c05msh7ee26c674f810e5p1df3e2jsn2e0a7d20d96b")
-                    .build()
-                val body = client.newCall(request).execute().body?.string()
-
-                val gson = GsonBuilder().create()
-
-                this.brands = gson.fromJson(body, Array<String>::class.java)
-            } catch (e: java.lang.Exception) {
-                e.printStackTrace()
-            }
-        }
-
-        thread.start()
-        thread.join()
     }
 }
 
