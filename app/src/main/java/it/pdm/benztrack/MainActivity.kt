db08d5bed8f0c815076e5bac894754634e9b7645
Nicolas Guarini <nikiguarincia@gmail.com>
2022-06-17 14:20:55 +0200

    Implement notification system which sends update notifications every 15 days

--- app/src/main/java/it/pdm/benztrack/MainActivity.kt
+++ app/src/main/java/it/pdm/benztrack/MainActivity.kt
@@ -1,9 +1,13 @@
 package it.pdm.benztrack
 
+import android.app.AlarmManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,8 +15,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val sharedPref = getSharedPreferences("it.pdm.benztrack", Context.MODE_PRIVATE)
-
         if(sharedPref.getBoolean("firstStart", true)){
+            setupAlarmManager()
             val intent = Intent(this, WelcomeActivity::class.java)
             startActivity(intent)
             finish()
@@ -21,5 +25,27 @@ class MainActivity : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
+    }
+
+    private fun setupAlarmManager(){
+        val calendar = Calendar.getInstance()
+        Log.d("MILLS", calendar.timeInMillis.toString())
+        Log.d("INTERVAL_DAY", AlarmManager.INTERVAL_DAY.toString())
+
+        val intent = Intent(applicationContext, NotificationReceiver::class.java)
+        val pendingIntent = PendingIntent.getBroadcast(
+            applicationContext,
+            100,
+            intent,
+            PendingIntent.FLAG_UPDATE_CURRENT
+        )
+
+        val alarmManager = getSystemService(ALARM_SERVICE) as AlarmManager
+        alarmManager.setRepeating(
+            AlarmManager.RTC_WAKEUP,
+            calendar.timeInMillis,
+            AlarmManager.INTERVAL_DAY * 15, //send notification every 15 days
+            pendingIntent
+        )
     }
 }
---
+++ app/src/main/java/it/pdm/benztrack/NotificationReceiver.kt
@@ -0,0 +1,87 @@
+package it.pdm.benztrack
+
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.math.roundToInt
+
+class NotificationReceiver : BroadcastReceiver() {
+    private val channelId = "BenzTrack Notifications"
+
+    @RequiresApi(Build.VERSION_CODES.M)
+    override fun onReceive(context: Context?, intent: Intent?) {
+        val sharedPreferences = context?.getSharedPreferences("it.pdm.benztrack", Context.MODE_PRIVATE)
+
+        if(sharedPreferences != null && sharedPreferences.getFloat("consumptionPrevMonth", -1f) != -1f){
+            val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            val repeatingIntent = Intent(context, DashboardActivity::class.java)
+            repeatingIntent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val channelId = channelId
+                val mChannel = NotificationChannel(
+                    channelId,
+                    "General Notifications",
+                    NotificationManager.IMPORTANCE_DEFAULT
+                )
+                mChannel.description = "This is default channel used for all other notifications"
+                notificationManager.createNotificationChannel(mChannel)
+            }
+
+            val pendingIntent = PendingIntent.getActivity(
+                context,
+                100,
+                repeatingIntent,
+                PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
+            )
+
+            val spentThisMonth = sharedPreferences.getFloat("spentThisMonth", -1f)
+            val emittedThisMonth = sharedPreferences.getFloat("emittedThisMonth", -1f)
+            val consumptionThisMonth = sharedPreferences.getFloat("consumptionThisMonth", -1f)
+            val consumptionPrevMonth = sharedPreferences.getFloat("consumptionPrevMonth", -1f)
+
+            val cal: Calendar = Calendar.getInstance()
+            val monthDate = SimpleDateFormat("MMMM", Locale.ITALIAN)
+            val monthYear = SimpleDateFormat("yyyy", Locale.ITALIAN)
+
+            val monthName: String = monthDate.format(cal.time)
+            val yearName: String = monthYear.format(cal.time)
+            val deltaPercent = (getDeltaPercent(consumptionPrevMonth, consumptionThisMonth) * 10.0).roundToInt() / 10.0
+            val contentTitle = "Aggiornamento consumi $monthName $yearName"
+            var contentText = "Questo mese hai speso $spentThisMonth €, emettendo $emittedThisMonth g di CO, con un consumo medio di $consumptionThisMonth km/l, "
+            contentText += if(deltaPercent < 0f){
+                "-$deltaPercent% rispetto al mese scorso, stai andando alla grande!"
+            }else{
+                "+$deltaPercent% rispetto al mese scorso, puoi fare di meglio!"
+            }
+
+            val builder = NotificationCompat.Builder(context, channelId)
+                .setContentIntent(pendingIntent)
+                .setSmallIcon(R.drawable.ic_logo)
+                .setContentTitle(contentTitle)
+                .setContentText(contentText)
+                .setStyle(NotificationCompat.BigTextStyle().bigText(contentText))
+                .setAutoCancel(true)
+
+            notificationManager.notify(100, builder.build())
+        }
+    }
+
+    private fun getDeltaPercent(consumption1: Float, consumption2: Float): Float{
+        return if(consumption1 > consumption2){
+            val delta = consumption1 - consumption2
+            -(delta / consumption1 * 100)
+        }else {
+            val delta = consumption2 - consumption1
+            delta / consumption2 * 100
+        }
+    }
+}
